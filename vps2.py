afc0fce8202bffd0f8a0177126633c71969d0680
sangrepollo <pollodesangre@gmail.com>
2023-02-03 11:51:25 -0500

    Update vps2.py

--- vps2.py
+++ vps2.py
@@ -1,31 +1,58 @@
-import asyncio
-from telethon import TelegramClient, errors
+importar asyncio
+
+de teletón import TelegramClient, errores
 
 api_id = 29577035
+
 api_hash = 'a1b79892e940a0033e36914534d01e70'
 
-client = TelegramClient('session_name', api_id, api_hash)
+cliente = TelegramClient('nombre_sesión', api_id, api_hash)
 
-async def main():
-    await client.start()
-     group_entities = [await client.get_entity('@MLC_isladelajuventud'),
-                      await client.get_entity('@compraisla'),
-                      await client.get_entity('@isla_venta'),
-                      await client.get_entity('@Isla_Ventas'),
-                      await client.get_entity('@ventasisladepinos'),
-                      await client.get_entity('@geronabusiness'),
-                      await client.get_entity('@mercadominoristameij'),
-                      await client.get_entity('@comprasyventasenlaisla')]
-    while True:
-        for group_entity in group_entities:
-            try:
-                await client.send_message(group_entity, '📯 BUSCO 📯\n\nMLC 150 💳‼️\nEURO 155 💶‼️\nUSD 150 💵‼️\n\n📫 DISPONIBLES https://wa.me/+5355806085 las 24 hrs, no dude escribir o llamar será bien atendido 📌\n\n🛎️ REMESAS 🛎️\n100 🇺🇸14500cup 🇨🇺\n112 🇺🇸 x 100mlc 🇨🇺\n110 🇺🇸x 100usd 🇨🇺\n\n🛡️ Nuestro servicio es de alta seguridad. 🛡️\n🛡️ Le brindamos a nuestros clientes la mejor atención para su comodidad y confianza. 🛡️\n🛡️ Todo presencial grupo https://chat.whatsapp.com/L1frnLZTSykG2JTqZnnVk9. 🛡️\n🛡️ Nos marca la buena referencia. 🛡️\n\n🚨🚩🚨\nCuide su dinero de imitadores y estafadores . 🚨🚩🚨')
+definición asíncrona principal ():
+
+    esperar cliente.start()
+
+    
+
+    group_entities = [esperar cliente.get_entity('@MLC_isladelajuventud'),
+
+                      esperar cliente.get_entity('@compraisla'),
+
+                      esperar cliente.get_entity('@isla_venta'),
+
+                      esperar cliente.get_entity('@Isla_Ventas'),
+
+                      aguardar cliente.get_entity('@ventasisladepinos'),
+
+                      esperar cliente.get_entity('@geronabusiness'),
+
+                      esperar cliente.get_entity('@mercadominoristameij'),
+
+                      esperar cliente.get_entity('@comprasyventasenlaisla')]
+
+    
+
+    mientras que es cierto:
+
+        para group_entity en group_entities:
+
+            intentar:
+
+                mensaje = '📯 BUSCO 📯\n\nMLC 150 💳‼️\nEURO 155 💶‼️\nUSD 150 💵‼️\n\n📫 DISPONIBLES https://wa.me/+5355806085 las 24 hrs, no dude escribir o llamar será bien atendido 📌\n\n🛎️ REMESAS 🛎️\n100 🇺🇸14500cup 🇨🇺\n112 🇺🇸 x 100mlc 🇨🇺\n110 🇺🇸x 100usd 🇨🇺\n\n🛡️. 🛡️\n🛡️ Le brindamos a nuestros clientes la mejor atención para su comodidad y confianza. 🛡️\n🛡️ Todo grupo presencial https://chat.whatsapp.com/L1frnLZTSykG2JTqZnnVk9. 🛡️\n'
+
+                esperar cliente. enviar_mensaje (entidad_grupo, mensaje)
+
                 await asyncio.sleep(60) # agrega una pausa de 60 segundos
-            except errors.UsernameNotOccupiedError:
-                # Si el usuario o grupo no existe, simplemente ignorar y continuar
-                pass
-        await asyncio.sleep(9488) # espera 2:48 horas 
 
-with client:
-    client.loop.run_until_complete(main())
+            excepto errores.UsernameNotOccupiedError:
+
+                # Si el usuario o grupo no existe, simplemente ignore y continúe
+
+                aprobar
+
+        esperar asyncio.sleep(9488) # espera 2:48 horas
+
+con el cliente:
+
+    cliente.bucle.ejecutar_hasta_completo(principal())
 
